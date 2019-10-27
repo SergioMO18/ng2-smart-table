8dd3f58e68e4637a2f6ee3ae8e8c4ca06458c585
Sergio Mendoza <sergio.mendoza@continex.es>
2019-10-27 23:18:49 +0100

    Revert "refactor: migrate tables to the new structure"
    This reverts commit d940e6ff2f2485758885b620156c0cd997139e44.

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,28 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { Ng2SmartTableModule } from 'ng2-smart-table';
 
-import { PagesModule } from './pages/pages.module';
 import { AppComponent } from './app.component';
-import { routes } from './app.routes';
-import { ScrollPositionDirective } from './theme/directives/scrollPosition.directive';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    ScrollPositionDirective,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    RouterModule.forRoot(routes, { useHash: true }),
-    Ng2SmartTableModule,
-    PagesModule,
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
